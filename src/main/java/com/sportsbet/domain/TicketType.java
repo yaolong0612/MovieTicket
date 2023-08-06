1d0334e982cd87fc06724811a612de9197918a36
yaolong <yaolong0612@gmail.com>
2023-08-06 13:54:01 +1000

    Adding a custom serialization value for each TicketType enum constant

--- src/main/java/com/sportsbet/domain/TicketType.java
+++ src/main/java/com/sportsbet/domain/TicketType.java
@@ -1,11 +1,24 @@
 package com.sportsbet.domain;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Enumeration representing different types of movie tickets.
  */
 public enum TicketType {
-    ADULT,
-    SENIOR,
-    TEEN,
-    CHILDREN;
+    ADULT("Adult"),
+    SENIOR("Senior"),
+    TEEN("Teen"),
+    CHILDREN("Children");
+
+    private final String value;
+
+    TicketType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
